8c5f8d67fceb782b3c7d189b54bc14443d56c535
Pushpitha <pushpithagee@gmail.com>
2024-04-22 09:18:40 +0530

    fixes : first load error

--- app/fleet/page.tsx
+++ app/fleet/page.tsx
@@ -3,7 +3,6 @@ import TableFilterForm from "./TableFilterForm"
 import { Card, Typography } from "../../UI/MaterialComponents"
 import { useSelector } from "react-redux"
 import { selectCurrentData } from "@/redux/slices/tableSlice"
-import { TableRow } from "@/redux/slices/tableSlice"
 import { renderPrefix } from "./TableFilterForm"
 
 const TABLE_HEAD = ["R.No", "Depot", "Make and Model", "Type", "R.from", "More"]
@@ -46,54 +45,61 @@ export default function Fleet() {
                   </tr>
                 </thead>
                 <tbody className="text-xs md:text-base">
-                  {data.map((r: any) => (
-                    <tr
-                      key={r.id}
-                      className="border-b-[1px] border-gray-700  text-gray-400"
-                    >
-                      <td className="p-4">
-                        <Typography variant="small" className="font-normal">
-                          {renderPrefix(r.RegPrefix?.prefix)} {r.R_No}
-                        </Typography>
-                      </td>
-                      <td className="p-4">
-                        <Typography variant="small" className="font-normal">
-                          {r.Depot?.name}
-                        </Typography>
-                      </td>
-                      <td className="p-4">
-                        <Typography variant="small" className="font-normal">
-                          {r.Model?.name}
-                        </Typography>
-                      </td>
-                      <td className="p-4">
-                        <Typography variant="small" className="font-normal">
-                          {r.Type.name}
-                        </Typography>
-                      </td>
-                      <td className="p-4">
-                        <Typography variant="small" className="font-normal">
-                          {r.R_from}
-                        </Typography>
-                      </td>
-                      <td className="p-4">
-                        {r.img_url && (
-                          <a href={r.img_url} target="_blank">
-                            <Typography variant="small" className="font-normal">
-                              Show Image
-                            </Typography>
-                          </a>
-                        )}
-                        {r.article_url && (
-                          <a href={r.article_url} target="_blank">
-                            <Typography variant="small" className="font-normal">
-                              Read Article
-                            </Typography>
-                          </a>
-                        )}
-                      </td>
-                    </tr>
-                  ))}
+                  {data.length &&
+                    data.map((r: any) => (
+                      <tr
+                        key={r.id}
+                        className="border-b-[1px] border-gray-700  text-gray-400"
+                      >
+                        <td className="p-4">
+                          <Typography variant="small" className="font-normal">
+                            {renderPrefix(r.RegPrefix?.prefix)} {r.R_No}
+                          </Typography>
+                        </td>
+                        <td className="p-4">
+                          <Typography variant="small" className="font-normal">
+                            {r.Depot?.name}
+                          </Typography>
+                        </td>
+                        <td className="p-4">
+                          <Typography variant="small" className="font-normal">
+                            {r.Model?.name}
+                          </Typography>
+                        </td>
+                        <td className="p-4">
+                          <Typography variant="small" className="font-normal">
+                            {r.Type.name}
+                          </Typography>
+                        </td>
+                        <td className="p-4">
+                          <Typography variant="small" className="font-normal">
+                            {r.R_from}
+                          </Typography>
+                        </td>
+                        <td className="p-4">
+                          {r.img_url && (
+                            <a href={r.img_url} target="_blank">
+                              <Typography
+                                variant="small"
+                                className="font-normal"
+                              >
+                                Show Image
+                              </Typography>
+                            </a>
+                          )}
+                          {r.article_url && (
+                            <a href={r.article_url} target="_blank">
+                              <Typography
+                                variant="small"
+                                className="font-normal"
+                              >
+                                Read Article
+                              </Typography>
+                            </a>
+                          )}
+                        </td>
+                      </tr>
+                    ))}
                 </tbody>
               </table>
             </div>
